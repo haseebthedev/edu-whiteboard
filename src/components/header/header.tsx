f9891f4846526b07acc74b350ed83c42a4d12713
haseebthedev <haseebhowto@gmail.com>
2024-11-25 20:09:09 +0500

    latest changes

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="whiteboard_header">
+      <div className="heading">Lesson Name: 1.1PV</div>
+    </div>
+  );
+};
+
+export { Header };
